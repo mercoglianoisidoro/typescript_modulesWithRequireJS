58e89d748ab6ea24cb6903643b8bd6296fa92741
Mercogliano Isidoro <mercogliano.isidoro@gmail.com>
2016-10-22 19:53:59 +0200

    requirejs import using typescript

--- web/configRequirejs.js
+++ web/configRequirejs.js
@@ -1,22 +1,22 @@
 /// <reference path="../typings/index.d.ts" />
-requirejs.config({
-    paths: {
-        "jquery": "../node_modules/jquery/dist/jquery",
-        "dataTables": "../node_modules/datatables.net/js/jquery.dataTables"
-    }
+define(["require", "exports", "./helloworld", "./secondModule"], function (require, exports, helloworld, secondModule) {
+    "use strict";
+    console.log(helloworld);
+    console.log(secondModule);
 });
-requirejs(["helloworld"]);
-requirejs(["secondModule"]);
+//import ajaxCall = require("./ajaxCall");console.log(ajaxCall);
 //example ajaxCall
-requirejs(['ajaxCall', 'helloworld'], function (ajaxCall) {
-    try {
-        var urlTest = "http://10.0.3.110/workspaces/workspace_php/mercoglianoisidoro_api/index.php/save_data";
-        var call = new ajaxCall(urlTest);
-        call.setMethod('POST');
-        call.setDataDoSend({ 'param1': "val1" });
-        console.log('result=', call.execute());
-    }
-    catch (error) {
-        console.error('error during call', error);
-    }
-});
+// requirejs(['ajaxCall','helloworld'], function(ajaxCall) {
+//
+//     try {
+//         var urlTest = "http://10.0.3.110/workspaces/workspace_php/mercoglianoisidoro_api/index.php/save_data";
+//
+//         var call = new ajaxCall(urlTest);
+//         call.setMethod('POST');
+//         call.setDataDoSend({'param1':"val1"})
+//
+//         console.log('result=', call.execute());
+//     } catch (error) {
+//         console.error('error during call', error);
+//     }
+// });
--- web/configRequirejs.ts
+++ web/configRequirejs.ts
@@ -1,32 +1,31 @@
 
 /// <reference path="../typings/index.d.ts" />
 
-requirejs.config({
-    paths: {
-        "jquery": "../node_modules/jquery/dist/jquery",
-        "dataTables": "../node_modules/datatables.net/js/jquery.dataTables"
-    }
-});
+ 
+//direct call to js function
+//requirejs(["helloworld"]);
+//requirejs(["secondModule"]);
 
+//using typescript import
+//require .config must be put outside this file
+import helloworld = require("./helloworld");console.log(helloworld);
+import secondModule = require("./secondModule");console.log(secondModule);
 
-requirejs(["helloworld"]);
-requirejs(["secondModule"]);
-
-
+//import ajaxCall = require("./ajaxCall");console.log(ajaxCall);
 
 
 //example ajaxCall
-requirejs(['ajaxCall','helloworld'], function(ajaxCall) {
-  
-    try {
-        var urlTest = "http://10.0.3.110/workspaces/workspace_php/mercoglianoisidoro_api/index.php/save_data";
-
-        var call = new ajaxCall(urlTest);
-        call.setMethod('POST');
-        call.setDataDoSend({'param1':"val1"})
-
-        console.log('result=', call.execute());
-    } catch (error) {
-        console.error('error during call', error);
-    }
-});
+// requirejs(['ajaxCall','helloworld'], function(ajaxCall) {
+//
+//     try {
+//         var urlTest = "http://10.0.3.110/workspaces/workspace_php/mercoglianoisidoro_api/index.php/save_data";
+//
+//         var call = new ajaxCall(urlTest);
+//         call.setMethod('POST');
+//         call.setDataDoSend({'param1':"val1"})
+//
+//         console.log('result=', call.execute());
+//     } catch (error) {
+//         console.error('error during call', error);
+//     }
+// });
